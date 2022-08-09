821842b7856218cc525384cca146426cb9ac0cb7
ZDannawi <zdannawi@symplr.com>
2022-08-08 21:33:42 -0400

    Updated run script to handle opening Cypress test runner as another option

--- runApp.sh
+++ runApp.sh
@@ -6,12 +6,16 @@ case $1 in
         echo "You may pass in the following options: "
         echo -e '\t1. Pass --chrome to run on Chrome.'
         echo -e '\t2. Pass --firefox to run on Firefox.'
+        echo -e '\t3. Pass --runner to open Cypress runner'
         ;;
     "--chrome")
-        docker run -it -v $PWD:/e2e -w /e2e cypress/included:10.4.0 --browser $1
+        docker run -it -v $PWD:/e2e -w /e2e cypress/included:10.4.0 --browser chrome
         ;;
     "--firefox")
-        docker run -it -v $PWD:/e2e -w /e2e cypress/included:10.4.0 --browser $1
+        docker run -it -v $PWD:/e2e -w /e2e cypress/included:10.4.0 --browser firefox
+        ;;
+    "--runner")
+        npx cypress open
         ;;
     *)
         echo "You have failed to specify a parameter. Pass the --help flag for options."
